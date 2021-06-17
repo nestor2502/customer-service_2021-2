a0fc873c2114699df545b1db953d32faf4dd5323
nestor2502 <nvazquez@techassist.mx>
2021-06-17 11:58:34 -0500

    feature CRUD_Region add dto region
    Se creo el dto de region
    
    Funcionalidad CRUD Region
    API:customer
    Responsable: Nestor Vazquez

---
+++ src/main/java/com/dws/customerservice/dto/Region.java
@@ -0,0 +1,13 @@
+package com.dws.customerservice.dto;
+
+public class Region {
+	
+	private int id;
+	private String region;
+	
+	public Region(int id,String region ) {
+		this.id = id;
+		this.region = region;
+	}
+
+}
